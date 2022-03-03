e22eddcc34320d20ef904b56b1fd7cc589e1e809
Pat Lawler <patlaw777@icloud.com>
2022-03-03 15:51:45 -0600

    fixed port issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { events } = require('./data/events');
 const app = express();
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 
 app.get('/api/events', (req, res) => {
